1b2dae000c3ade659fde59d38e157b53c20d5c8c
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 00:48:49 +0100

    Reconstruction of GUI

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Import PySide6 classes
 import sys
 from PySide6 import QtCore, QtWidgets
+import version
+
+print(f'Version: {version.version}')
+print(f'Autor: {version.author}')
+print(f'AppName: {version.app_name}')
 
 # Create a Qt application
 app = QtWidgets.QApplication(sys.argv)
@@ -8,7 +13,7 @@ app = QtWidgets.QApplication(sys.argv)
 # Create a Window
 mywindow = QtWidgets.QWidget()
 mywindow.resize(600, 800)
-mywindow.setWindowTitle('Hello, World!')
+mywindow.setWindowTitle(version.app_name)
 
 # Create a label and display it all together
 mylabel = QtWidgets.QLabel(mywindow)
---
+++ version.py
@@ -0,0 +1,3 @@
+app_name = "MaCAN"
+version = "0.1.1"
+author = "Thomas Fischer from indietonne.de"
