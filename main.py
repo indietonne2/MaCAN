84224166f241eb06eb91416a5a35c974bcf6de44
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 01:15:08 +0100

    Revert "fixed typo"
    This reverts commit 9ab98df77595d09e6b69f4ceead3645e1988f938.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+# Import PySide6 classes
 import sys
-from PySide6.QtCore import QRect
 from PySide6.QtWidgets import QMainWindow, QApplication, QAction, QLabel, QMenuBar, QMenu
 import version
 
@@ -7,24 +7,34 @@ print(f'Version: {version.version}')
 print(f'Autor: {version.author}')
 print(f'AppName: {version.app_name}')
 
+# Create a Qt application
 app = QApplication(sys.argv)
 
 
+# Create a Window
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setGeometry(50, 50, 1920, 1080)
         self.setWindowTitle(f"{version.app_name} {version.version}")
+
+        # Create menu bar
         self.menu_bar = QMenuBar(self)
         self.setMenuBar(self.menu_bar)
+
+        # Create a File menu
         self.file_menu = QMenu('File', self)
         self.menu_bar.addMenu(self.file_menu)
+
+        # Add actions to File menu
         self.exit_action = QAction('Exit', self)
         self.file_menu.addAction(self.exit_action)
         self.exit_action.triggered.connect(self.exit_app)
+
+        # Create a label
         self.mylabel = QLabel(self)
         self.mylabel.setText('Hello, World!')
-        self.mylabel.setGeometry(QRect(200, 200, 200, 200))
+        self.mylabel.setGeometry(QtCore.QRect(200, 200, 200, 200))
 
     def exit_app(self):
         sys.exit()
@@ -33,4 +43,5 @@ class MainWindow(QMainWindow):
 mywindow = MainWindow()
 mywindow.show()
 
+# Enter Qt application main loop
 sys.exit(app.exec())
