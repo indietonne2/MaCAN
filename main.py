871f895b3df721ba3b7bcdc10a71d83c7561079b
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 07:01:53 +0100

    menu bar only liten up but no funciton

--- main.py
+++ main.py
@@ -1,81 +1,61 @@
 import sys
-from PySide6.QtCore import QRect
-from PySide6.QtWidgets import QMainWindow, QApplication, QAction, QLabel, QMenu, QWidget, QVBoxLayout, QToolBar, QLineEdit, QToolButton
 import version
-
-print(f'Version: {version.version}')
-print(f'Autor: {version.author}')
-print(f'AppName: {version.app_name}')
-
-app = QApplication(sys.argv)
+from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QMenu, QWidget, QVBoxLayout, QToolBar, QLineEdit, QToolButton
+from PySide6.QtGui import QAction
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        super().__init__()
         self.setGeometry(50, 50, 800, 600)
         self.setWindowTitle(f"{version.app_name} {version.version}")
 
-        # Setup Central Widget (main area)
         self.main_widget = QWidget(self)
+        self.layout = QVBoxLayout(self.main_widget)
         self.setCentralWidget(self.main_widget)
-        self.layout = QVBoxLayout()
-        self.main_widget.setLayout(self.layout)
 
-        # Add a QLineEdit to illustrate 'Cut' and 'Copy'
         self.text_edit = QLineEdit()
         self.layout.addWidget(self.text_edit)
 
-        # Setup Tool Bar
         self.tool_bar = self.addToolBar('Main Tools')
 
-        # Setup File Menu on tool bar
-        self.file_menu = QMenu('File', self)
         self.file_button = QToolButton(self)
+        self.file_button.setText('File')
+        self.file_menu = QMenu('File', self.file_button)
         self.file_button.setMenu(self.file_menu)
         self.file_button.setPopupMode(QToolButton.InstantPopup)
-        self.file_button.setText('File')
         self.tool_bar.addWidget(self.file_button)
 
-        # Setup Edit Menu on tool bar
-        self.edit_menu = QMenu('Edit', self)
         self.edit_button = QToolButton(self)
+        self.edit_button.setText('Edit')
+        self.edit_menu = QMenu('Edit', self.edit_button)
         self.edit_button.setMenu(self.edit_menu)
         self.edit_button.setPopupMode(QToolButton.InstantPopup)
-        self.edit_button.setText('Edit')
         self.tool_bar.addWidget(self.edit_button)
 
-        # Setup Help Menu on tool bar
-        self.help_menu = QMenu('Help', self)
         self.help_button = QToolButton(self)
+        self.help_button.setText('Help')
+        self.help_menu = QMenu('Help', self.help_button)
         self.help_button.setMenu(self.help_menu)
         self.help_button.setPopupMode(QToolButton.InstantPopup)
-        self.help_button.setText('Help')
         self.tool_bar.addWidget(self.help_button)
 
-        # File Menu Actions
         self.exit_action = QAction('Exit', self)
+        self.exit_action.triggered.connect(self.close)
         self.file_menu.addAction(self.exit_action)
-        self.exit_action.triggered.connect(self.exit_app)
 
-        # Edit Menu Actions
         self.cut_action = QAction('Cut', self)
-        self.edit_menu.addAction(self.cut_action)
         self.cut_action.triggered.connect(self.text_edit.cut)
+        self.edit_menu.addAction(self.cut_action)
 
         self.copy_action = QAction('Copy', self)
-        self.edit_menu.addAction(self.copy_action)
         self.copy_action.triggered.connect(self.text_edit.copy)
-
-        # Help Menu Actions
-        self.about_action = QAction('About', self)
-        self.help_menu.addAction(self.about_action)
+        self.edit_menu.addAction(self.copy_action)
 
         self.mylabel = QLabel(self.main_widget)
         self.mylabel.setText('Hello, World!')
 
-    def exit_app(self):
-        sys.exit()
-
-
-mywindow = MainWindow()
-mywindow.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 app_name = "MaCAN"
-version = "0.1.11"
+version = "0.1.12"
 author = "Thomas Fischer from indietonne.de"
