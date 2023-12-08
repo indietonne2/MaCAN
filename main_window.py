6dc070725ec326fefdac6119bf82d05d7101245d
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 20:39:44 +0100

    Colors of selection bar

--- main_window.py
+++ main_window.py
@@ -1,69 +1,95 @@
+import configparser
 from PySide6.QtWidgets import QMainWindow, QLabel, QApplication, QVBoxLayout, QWidget, QComboBox
 from PySide6.QtGui import QColor, QPixmap, QPainter, QPaintEvent
 from PySide6.QtCore import Qt
 import sys
 import os
-import configparser
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.config = configparser.ConfigParser()
-        self.modus_config = self.read_config()
-        self.background_image = QLabel(self)  # Hintergrundbild Label
-        self.color_bar = QLabel(self)  # Farbbalken
+        self.parameter_config = configparser.ConfigParser()
+        self.mode_config = configparser.ConfigParser()
+        self.read_parameter_config()
         self.initUI()
+        self.read_mode_config()  # Liest den initialen Modus aus mode.cfg
 
-    def read_config(self):
-        self.config.read('parameter.cfg')
-        return self.config['Modus']
+    def read_parameter_config(self):
+        self.parameter_config.read('parameter.cfg')
+        return self.parameter_config['Modus']
+
+    def read_mode_config(self):
+        self.mode_config.read('mode.cfg')
+        selected_mode = self.mode_config.get('selected mode', 'mode', fallback=None)
+        if selected_mode and selected_mode in self.parameter_config:
+            self.combo_box.setCurrentText(selected_mode)
+            self.update_color_bar_and_text(selected_mode)
+        else:
+            first_mode = next(iter(self.parameter_config['Modus']))
+            self.combo_box.setCurrentText(first_mode)
+            self.update_color_bar_and_text(first_mode)
+
+    def write_mode_config(self, mode):
+        self.mode_config['selected mode'] = {'mode': mode}
+        with open('mode.cfg', 'w') as configfile:
+            self.mode_config.write(configfile)
 
     def initUI(self):
         self.setWindowTitle("Modus Auswahl")
         self.setGeometry(100, 100, 800, 600)
 
-        self.update_background_image()
-
         layout = QVBoxLayout()
 
         # Farbbalken
-        self.color_bar.setFixedHeight(20)  # Setzt die Höhe des Farbbalkens auf 20 Pixel
-        self.color_bar.setAlignment(Qt.AlignCenter)  # Text zentrieren
+        self.color_bar = QLabel(self)
+        self.color_bar.setFixedHeight(20)
+        self.color_bar.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.color_bar)
 
         # Auswahlfeld
         self.combo_box = QComboBox(self)
         self.fill_combo_box()
-        self.combo_box.currentTextChanged.connect(self.update_color_bar_and_text)  # Aktualisieren der Farbe und Text bei Auswahl
+        self.combo_box.currentTextChanged.connect(self.on_mode_selected)
         layout.addWidget(self.combo_box, 0, Qt.AlignTop)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+        self.update_background_image()
+
     def fill_combo_box(self):
-        for key in self.modus_config.keys():
+        for key in self.parameter_config['Modus']:
             self.combo_box.addItem(key)
 
+    def on_mode_selected(self, text):
+        self.update_color_bar_and_text(text)
+        self.write_mode_config(text)
+
     def update_color_bar_and_text(self, text):
-        color_name = self.modus_config.get(text, "Rot")
-        color_map = {"Rot": QColor(255, 0, 0), "Grün": QColor(0, 255, 0), "Blau": QColor(0, 0, 255), "Orange": QColor(255, 165, 0)}
-        color = color_map[color_name]
+        color_name = self.parameter_config['Modus'].get(text, "Rot")
+        color_map = {
+            "Rot": QColor(255, 0, 0),
+            "Grün": QColor(0, 255, 0),
+            "Blau": QColor(0, 0, 255),
+            "Orange": QColor(255, 165, 0)
+        }
+        color = color_map.get(color_name, QColor(255, 0, 0))
         text_color = "white" if self.is_color_dark(color) else "black"
         self.color_bar.setStyleSheet(f"background-color: {color.name()}; color: {text_color};")
-        self.color_bar.setText(text)  # Setzt den ausgewählten Modus als Text im Farbbalken
+        self.color_bar.setText(text)
 
     def is_color_dark(self, color: QColor):
-        # Berechnung der Luminanz
         luminance = (0.299 * color.red() + 0.587 * color.green() + 0.114 * color.blue()) / 255
         return luminance < 0.5
 
     def update_background_image(self):
         pixmap = QPixmap(os.path.join('resources', 'background.png'))
         painter = QPainter(pixmap)
-        painter.setOpacity(0.5)  # Setzt die Transparenz auf 50%
+        painter.setOpacity(0.5)
         painter.drawPixmap(pixmap.rect(), pixmap)
         painter.end()
+        self.background_image = QLabel(self)
         self.background_image.setPixmap(pixmap)
         self.background_image.setGeometry(0, self.color_bar.height(), self.width(), self.height() - self.color_bar.height())
 
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 app_name = "MaCAN"
-version = "0.6.8"
+version = "0.6.9"
 author = "Thomas Fischer from indietonne.de"
