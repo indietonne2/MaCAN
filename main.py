aba7a94642ba7759681e94d4c4cc00c9345ad96b
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 06:58:34 +0100

    menu bar only liten up but no funciton

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from PySide6.QtCore import QRect
-from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QMenuBar, QMenu, QWidget, QVBoxLayout, QToolBar, QLineEdit
-from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow, QApplication, QAction, QLabel, QMenu, QWidget, QVBoxLayout, QToolBar, QLineEdit, QToolButton
 import version
 
 print(f'Version: {version.version}')
@@ -10,14 +9,13 @@ print(f'AppName: {version.app_name}')
 
 app = QApplication(sys.argv)
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setGeometry(50, 50, 800, 600)
         self.setWindowTitle(f"{version.app_name} {version.version}")
 
-        # Setup Main Area (central widget)
+        # Setup Central Widget (main area)
         self.main_widget = QWidget(self)
         self.setCentralWidget(self.main_widget)
         self.layout = QVBoxLayout()
@@ -32,18 +30,27 @@ class MainWindow(QMainWindow):
 
         # Setup File Menu on tool bar
         self.file_menu = QMenu('File', self)
-        self.file_action = self.tool_bar.addAction('File')
-        self.file_action.setMenu(self.file_menu)
+        self.file_button = QToolButton(self)
+        self.file_button.setMenu(self.file_menu)
+        self.file_button.setPopupMode(QToolButton.InstantPopup)
+        self.file_button.setText('File')
+        self.tool_bar.addWidget(self.file_button)
 
         # Setup Edit Menu on tool bar
         self.edit_menu = QMenu('Edit', self)
-        self.edit_action = self.tool_bar.addAction('Edit')
-        self.edit_action.setMenu(self.edit_menu)
+        self.edit_button = QToolButton(self)
+        self.edit_button.setMenu(self.edit_menu)
+        self.edit_button.setPopupMode(QToolButton.InstantPopup)
+        self.edit_button.setText('Edit')
+        self.tool_bar.addWidget(self.edit_button)
 
         # Setup Help Menu on tool bar
         self.help_menu = QMenu('Help', self)
-        self.help_action = self.tool_bar.addAction('Help')
-        self.help_action.setMenu(self.help_menu)
+        self.help_button = QToolButton(self)
+        self.help_button.setMenu(self.help_menu)
+        self.help_button.setPopupMode(QToolButton.InstantPopup)
+        self.help_button.setText('Help')
+        self.tool_bar.addWidget(self.help_button)
 
         # File Menu Actions
         self.exit_action = QAction('Exit', self)
@@ -53,11 +60,11 @@ class MainWindow(QMainWindow):
         # Edit Menu Actions
         self.cut_action = QAction('Cut', self)
         self.edit_menu.addAction(self.cut_action)
-        self.cut_action.triggered.connect(self.text_edit.cut)  # Connect 'Cut' to QLineEdit.cut
+        self.cut_action.triggered.connect(self.text_edit.cut)
 
         self.copy_action = QAction('Copy', self)
         self.edit_menu.addAction(self.copy_action)
-        self.copy_action.triggered.connect(self.text_edit.copy)  # Connect 'Copy' to QLineEdit.copy
+        self.copy_action.triggered.connect(self.text_edit.copy)
 
         # Help Menu Actions
         self.about_action = QAction('About', self)
@@ -72,5 +79,3 @@ class MainWindow(QMainWindow):
 
 mywindow = MainWindow()
 mywindow.show()
-
-sys.exit(app.exec())
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 app_name = "MaCAN"
-version = "0.1.10"
+version = "0.1.11"
 author = "Thomas Fischer from indietonne.de"
