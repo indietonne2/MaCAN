1f1093626ad005420fcb6622c5f820e3cc91d96b
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 13:13:18 +0100

    refactored and fixed a lot of bugs. changed to own virtual environment

--- GUI/main.py
+++ GUI/main.py
@@ -1,73 +1,45 @@
-# Importing necessary libraries from PySide6
-import sys
-import PySide6
-
-
-# Importing version information
+from PySide6.QtWidgets import QMainWindow, QApplication, QStatusBar, QTextEdit, QAction, QMenu, QMenuBar, QRect
+from PySide6.QtCore import Qt
 from version import app_name, version, author
 
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
-class MaCANApp(PySide6.QtWidgets.QMainWindow):
+
+class MaCANApp(QMainWindow):
     """
-
     This class represents the main application window for MaCANApp.
-
-    Methods:
-        - __init__(self): Initializes the MaCANApp instance.
-        - create_widgets(self): Creates and configures the widgets for the GUI.
-
-    Attributes:
-        - main_content: Represents the main content area of the application as a QTextEdit widget.
-
-    Example usage:
-
-        app = MaCANApp()
-        app.show()
-
     """
+
     def __init__(self):
         super().__init__()
-
-        # Debugging print statement
-        self.main_content = None
-        print("Initializing MaCANApp")
-
-        # Setting the window title with the app name and version number
         self.setWindowTitle(f"{app_name} - v{version}")
-
-        # Call to create widgets for the GUI
+        self.main_content = None
         self.create_widgets()
 
     def create_widgets(self):
-        # Debugging print statementpip install PySide6
-        print("Creating widgets")
+        self.create_menu_bar()
+        self.create_status_bar()
+        self.create_main_content()
 
-        # Creating a menu bar
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu('&File')
+    def create_menu_bar(self):
+        menu_bar: QMenuBar = self.menuBar()
+        file_menu: QMenu = menu_bar.addMenu('&File')
+        file_menu.addAction(QAction('&Exit', self, triggered=self.close))
 
-        # Creating a file menu with an exit option
-        exit_action = QAction('&Exit', self)
-        exit_action.triggered.connect(self.close)
-        file_menu.addAction(exit_action)
-
-        # Creating a status bar to display the author information
-        status_bar = PySide6.QtWidgets.QStatusBar()
+    def create_status_bar(self):
+        status_bar: QStatusBar = QStatusBar()
         status_bar.showMessage(f"Developed by {author}")
         self.setStatusBar(status_bar)
 
-        # Creating the main content area as a text widget
-        self.main_content = PySide6.QtWidgets.QTextEdit(self)
+    def create_main_content(self):
+        self.main_content: QTextEdit = QTextEdit(self)
         self.main_content.setGeometry(QRect(10, 30, 780, 560))
 
 
 if __name__ == "__main__":
-    # Debugging print statement
-    print("Starting MaCANApp")
-
-    app = PySide6.QtWidgets.QApplication(sys.argv)
+    app = QApplication([])
     mainWin = MaCANApp()
-    mainWin.resize(800, 600)
+    mainWin.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
     mainWin.show()
-
-    sys.exit(app.exec())
+    app.exec()
--- version.py
+++ version.py
@@ -1,3 +1,3 @@
 app_name = "MaCAN"
-version = "0.3.0"
+version = "0.3.1"
 author = "Thomas Fischer from indietonne.de"
