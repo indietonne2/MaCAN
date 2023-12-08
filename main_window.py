78ef22e11a56b3354c6d5fbfe6c369652cf112e2
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 19:55:58 +0100

    Background image

--- main_window.py
+++ main_window.py
@@ -1,50 +1,54 @@
 import configparser
-from PySide6.QtWidgets import QMainWindow, QLabel, QApplication, QVBoxLayout, QWidget, QPushButton, QSpacerItem, QSizePolicy
+from PySide6.QtWidgets import QMainWindow, QLabel, QApplication, QVBoxLayout, QWidget, QComboBox
 from PySide6.QtGui import QColor, QPixmap, QPainter, QPaintEvent
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import Qt
 import sys
 import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.config = configparser.ConfigParser()
         self.modus_config = self.read_config()
         self.background_image = QLabel(self)  # Hintergrundbild Label
+        self.color_bar = QLabel(self)  # Farbbalken
         self.initUI()
 
     def read_config(self):
-        config = configparser.ConfigParser()
-        config.read('parameter.cfg')
-        return config['Modus']
+        self.config.read('parameter.cfg')
+        return self.config['Modus']
 
     def initUI(self):
-        modus = "Live"
-        farbe = self.modus_config.get(modus, "Rot")
-
-        self.setWindowTitle(f"{modus} Modus")
+        self.setWindowTitle("Modus Auswahl")
         self.setGeometry(100, 100, 800, 600)
-
-        color_map = {"Rot": QColor(255, 0, 0), "Grün": QColor(0, 255, 0), "Blau": QColor(0, 0, 255), "Orange": QColor(255, 165, 0)}
-
-        modus_label = QLabel(modus, self)
-        modus_label.setAlignment(Qt.AlignCenter)
-        modus_label.setStyleSheet(f"background-color: {color_map[farbe].name()};")
-        modus_label.setGeometry(0, 0, self.width(), 30)
 
         self.update_background_image()
 
         layout = QVBoxLayout()
-        layout.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))  # Dehnbarer Bereich oben
 
-        button = QPushButton("Select a Context", self)
-        button.clicked.connect(self.select_context)
-        layout.addWidget(button, 0, Qt.AlignCenter)
+        # Farbbalken
+        self.color_bar.setGeometry(0, 0, self.width(), 30)
+        self.color_bar.setStyleSheet("background-color: red;")  # Standardfarbe
+        layout.addWidget(self.color_bar)
 
-        layout.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))  # Dehnbarer Bereich unten
+        # Auswahlfeld
+        self.combo_box = QComboBox(self)
+        self.fill_combo_box()
+        self.combo_box.currentTextChanged.connect(self.update_color_bar)  # Aktualisieren der Farbe bei Auswahl
+        layout.addWidget(self.combo_box, 0, Qt.AlignTop)
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+
+    def fill_combo_box(self):
+        for key in self.modus_config.keys():
+            self.combo_box.addItem(key)
+
+    def update_color_bar(self, text):
+        color = self.modus_config.get(text, "Rot")
+        color_map = {"Rot": QColor(255, 0, 0), "Grün": QColor(0, 255, 0), "Blau": QColor(0, 0, 255), "Orange": QColor(255, 165, 0)}
+        self.color_bar.setStyleSheet(f"background-color: {color_map[color].name()};")
 
     def update_background_image(self):
         pixmap = QPixmap(os.path.join('resources', 'background.png'))
@@ -58,10 +62,6 @@ class MainWindow(QMainWindow):
     def resizeEvent(self, event: QPaintEvent):
         self.update_background_image()
         super().resizeEvent(event)
-
-    @Slot()
-    def select_context(self):
-        pass  # Ihre Implementierung hier
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
