01243978df63ce180a5ef15b04b7f88968be2c17
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 07:08:01 +0100

    fixed menu

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import sys
+from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QVBoxLayout, QAction, QMenu, QToolButton, QWidget, QToolBar, QLineEdit
+from PySide6.QtGui import QApplication
 import version
-from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QMenu, QWidget, QVBoxLayout, QToolBar, QLineEdit, QToolButton
-from PySide6.QtGui import QAction
+
+
+class MenuBar:
+    def __init__(self, parent):
+        self.tool_bar = QToolBar('Main Tools', parent)
+
+        self.create_file_menu()
+        self.create_edit_menu()
+
+    def create_file_menu(self):
+        file_button = QToolButton(self.tool_bar)
+        file_button.setText('File')
+        file_menu = QMenu(file_button)
+        file_button.setMenu(file_menu)
+        file_button.setPopupMode(QToolButton.InstantPopup)
+        self.tool_bar.addWidget(file_button)
+
+        exit_action = QAction('Exit', self.tool_bar)
+        exit_action.triggered.connect(QApplication.instance().quit)
+        file_menu.addAction(exit_action)
+
+    def create_edit_menu(self):
+        edit_button = QToolButton(self.tool_bar)
+        edit_button.setText('Edit')
+        edit_menu = QMenu(edit_button)
+        edit_button.setMenu(edit_menu)
+        edit_button.setPopupMode(QToolButton.InstantPopup)
+        self.tool_bar.addWidget(edit_button)
+
+        cut_action = QAction('Cut', self.tool_bar)
+        cut_action.triggered.connect(self.tool_bar.parent().parent().text_edit.cut)
+        edit_menu.addAction(cut_action)
+
+        copy_action = QAction('Copy', self.tool_bar)
+        copy_action.triggered.connect(self.tool_bar.parent().parent().text_edit.copy)
+        edit_menu.addAction(copy_action)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,40 +53,8 @@ class MainWindow(QMainWindow):
         self.text_edit = QLineEdit()
         self.layout.addWidget(self.text_edit)
 
-        self.tool_bar = self.addToolBar('Main Tools')
-
-        self.file_button = QToolButton(self)
-        self.file_button.setText('File')
-        self.file_menu = QMenu('File', self.file_button)
-        self.file_button.setMenu(self.file_menu)
-        self.file_button.setPopupMode(QToolButton.InstantPopup)
-        self.tool_bar.addWidget(self.file_button)
-
-        self.edit_button = QToolButton(self)
-        self.edit_button.setText('Edit')
-        self.edit_menu = QMenu('Edit', self.edit_button)
-        self.edit_button.setMenu(self.edit_menu)
-        self.edit_button.setPopupMode(QToolButton.InstantPopup)
-        self.tool_bar.addWidget(self.edit_button)
-
-        self.help_button = QToolButton(self)
-        self.help_button.setText('Help')
-        self.help_menu = QMenu('Help', self.help_button)
-        self.help_button.setMenu(self.help_menu)
-        self.help_button.setPopupMode(QToolButton.InstantPopup)
-        self.tool_bar.addWidget(self.help_button)
-
-        self.exit_action = QAction('Exit', self)
-        self.exit_action.triggered.connect(self.close)
-        self.file_menu.addAction(self.exit_action)
-
-        self.cut_action = QAction('Cut', self)
-        self.cut_action.triggered.connect(self.text_edit.cut)
-        self.edit_menu.addAction(self.cut_action)
-
-        self.copy_action = QAction('Copy', self)
-        self.copy_action.triggered.connect(self.text_edit.copy)
-        self.edit_menu.addAction(self.copy_action)
+        self.menu_bar = MenuBar(self)
+        self.addToolBar(self.menu_bar.tool_bar)
 
         self.mylabel = QLabel(self.main_widget)
         self.mylabel.setText('Hello, World!')
