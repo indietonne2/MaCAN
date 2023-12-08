c66f5fa0d8915ccb4165cfd01c4bbf3787a5996c
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 14:38:12 +0100

    Menu bar fix

--- custom_toolbar.py
+++ custom_toolbar.py
@@ -3,7 +3,6 @@ from PySide6.QtWidgets import QToolBar, QWidget, QSizePolicy
 class CustomToolBar(QToolBar):
     def __init__(self, title, parent=None):
         super().__init__(title, parent)
-        self.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
 
         # Spacer, der am Ende der Toolbar hinzugefügt wird, um die Buttons linksbündig zu machen
         spacer = QWidget()
