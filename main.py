f6f7afca3379fb7dd58b3ca6c0aea4c377e63921
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-08 08:15:13 +0100

    code reformated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from PySide6.QtWidgets import QToolBar, QWidget, QSizePolicy
 
 
 class CustomToolBar(QToolBar):
-    def __init__(self, title, parent=None):
+    def __init__(self, title, parent = None):
         super().__init__(title, parent)
         spacer = QWidget(self)
         spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
