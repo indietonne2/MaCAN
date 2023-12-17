9422e53ecc6df63b48d3678b87644308dde0bc23
indietonne2 <117590463+indietonne2@users.noreply.github.com>
2023-12-17 12:32:22 +0100

    Update application version and GUI elements
    This commit updates the application version in `version.py`, removes the live mode from `mode.cfg`, adds `emulation.ui` and `emulation_ui.py` for GUI purposes, and modifies the `main_window.py` to accommodate changes. The mode selection process has been adjusted and a dialog box is now conditionally shown when the mode "Emulation" is selected.

--- main_window.py
+++ main_window.py
@@ -1,3 +1,4 @@
+# Checking the provided code for necessary changes and updating it if needed
 import sys
 import os
 import importlib
@@ -16,10 +17,10 @@ class BackgroundWidget(QWidget):
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setOpacity(0.2)  # Set transparency
+        painter.setOpacity(0.2)
         pixmap_rect = self.pixmap.rect()
         target_rect = self.rect()
-        pixmap_rect.moveCenter(target_rect.center())  # Center the background image
+        pixmap_rect.moveCenter(target_rect.center())
         painter.drawPixmap(target_rect, self.pixmap, pixmap_rect)
         painter.end()
 
@@ -37,66 +38,77 @@ class MainWindow(QMainWindow):
         self.initUI()
         self.conditionally_show_emulation_dialog()
 
-        # Create an instance of MenuBar and add it to the main window
         self.menu_bar = MenuBar(self)
         self.addToolBar(self.menu_bar.tool_bar)
 
     def initUI(self):
-        self.setWindowTitle("Modus Auswahl")
+        self.setWindowTitle(\"Modus Auswahl\")
         self.setGeometry(100, 100, 800, 600)
 
-        self.background_widget = BackgroundWidget(self)
-        self.setCentralWidget(self.background_widget)
-
         self.main_layout = QVBoxLayout()
-        self.background_widget.setLayout(self.main_layout)
 
-        # Create the colored line
         self.color_line = QLabel(self)
         self.color_line.setFixedHeight(20)
         self.update_color_line(self.selected_mode)
         self.main_layout.addWidget(self.color_line)
 
-        # Create the mode dropdown
         self.mode_dropdown = QComboBox(self)
         self.mode_dropdown.addItems(self.parameter_config.keys())
         self.mode_dropdown.setCurrentText(self.selected_mode)
         self.mode_dropdown.currentTextChanged.connect(self.on_mode_changed)
         self.main_layout.addWidget(self.mode_dropdown)
 
-        # Other UI elements setup...
+        central_widget = QWidget(self)
+        central_widget.setLayout(self.main_layout)
+        self.setCentralWidget(central_widget)
 
     def read_mode_config(self):
         config = configparser.ConfigParser()
         config.read('mode.cfg')
-        # Convert mode to a consistent case, e.g., all lowercase
         return config['selected mode']['mode'].lower()
-
-    def get_mode_color(self, mode):
-        # Convert mode to the case used in parameter.cfg, e.g., capitalize the first letter
-        mode_formatted = mode.capitalize()
-        return self.parameter_config.get('Modus', mode_formatted, fallback="gray")
 
     def update_color_line(self, mode):
         color = self.get_mode_color(mode)
-        self.color_line.setStyleSheet(f"background-color: {color};")
+        self.color_line.setStyleSheet(f\"background-color: {color};\")
         self.color_line.setText(mode.capitalize())
+
+    def get_mode_color(self, mode):
+        config = configparser.ConfigParser()
+        config.read('parameter.cfg')
+        try:
+            color = config['Modus'][mode].split(' = ')[0]
+        except KeyError:
+            color = \"gray\"
+        return color
 
     def on_mode_changed(self, mode):
         self.update_color_line(mode)
         self.update_mode_config(mode)
-        # Additional logic for mode change...
+
+    def update_mode_config(self, new_mode):
+        config = configparser.ConfigParser()
+        config.read('mode.cfg')
+        config['selected mode'] = {'mode': new_mode}
+        with open('mode.cfg', 'w') as configfile:
+            config.write(configfile)
 
     def conditionally_show_emulation_dialog(self):
-        # Check if the selected mode is 'Emulation'
         if self.selected_mode.lower() == 'emulation':
             self.emulation_dialog = EmulationDialog(self)
             self.emulation_dialog.show()
 
-    # Rest of the MainWindow class methods...
-
-if __name__ == "__main__":
+if __name__ == \"__main__\":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+"""
+
+# Adding the update_mode_config method to the provided code
+updated_main_window_content = provided_main_window_content.replace(
+    "self.update_mode_config(mode)",
+    "self.update_mode_config(mode)"
+)
+
+# Display the updated content
+updated_main_window_content
